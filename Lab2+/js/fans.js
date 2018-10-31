a22c5684118e9847e94a62011ac8b4b8d5a84671
Nazar Gorokhivskiy <nazargorokhivskiy@gmail.com>
2018-10-31 11:25:47 +0200

    added code for 8th lab

--- Lab2+/js/fans.js
+++ Lab2+/js/fans.js
@@ -1,5 +1,15 @@
 const getById = id => document.getElementById(id);
 
+// global constants
+const localStorage = window.localStorage;
+const feedbackTextarea = getById("feedback-text");
+const feedbackContainer = getById("feedback-container");
+
+
+const isOnline = () => {
+    return window.navigator.onLine;
+}
+
 const feedbackTemplate = (text, date, time, nickname) => ` 
     <div class="request">
         <p>${text}</p>
@@ -10,21 +20,88 @@ const feedbackTemplate = (text, date, time, nickname) => `
     </div>
 `
 
+const reportNetworkStatus = () => {
+    if (isOnline()) {//if navigator.online == true
+        console.log("Network status: ONLINE");
+    } else {
+        console.log("Network status: offline");
+    }
+}
+
 function sendFeedback() {
 	var username = "Anonym";
-    var feedbackTextarea = getById("feedback-text");
-	var feedbackContainer = getById("feedback-container");
 
-    if (feedbackTextarea.value.length == 0) {
-    	alert("Please add feedback description");
-    	return;
+    if (feedbackTextarea.value.replace(/\s/g, '').length == 0) {
+        feedbackTextarea.classList.add("error-textarea");
+        feedbackTextarea.value = '';
+        feedbackTextarea.placeholder = "Please, add description";
+        return;
+    } else {
+        feedbackTextarea.classList.remove("error-textarea");
     }
-
+    
     var date = new Date();
+    var feedback = [
+        feedbackTextarea.value, date.toLocaleDateString(), date.toLocaleTimeString(), username
+    ];
 
+    storeMessage(feedback);
+}
+
+
+function storeMessage(elem) {
+    if (isOnline()) {
+        storeMessageRemotely(elem);
+    } else {
+        storeMessageLocaly(elem);
+    }
+}
+
+function storeMessageLocaly(elem) {
+    addToLocalStorage(elem, "feedback")
+    clearUI();
+    logEvent('Message saved locally: ' + elem[0]);
+}
+
+//TODO: Implement after learning Node.js
+function storeMessageRemotely(elem) {
+    clearUI();
+    console.log('Message sent to server:');    
+}
+
+
+function displayFromLocalStorage() {
+    var feedbacks = [];
+    getAllItems((resultArray) => {
+        feedbacks = resultArray;
+    }, "feedback")
+
+    for (var i = 0; i < feedbacks.length; i++) {
+        displayFeedback(feedbacks[i]);
+    }
+    deleteAllItems("feedback");
+}
+
+function displayFeedback(feedback) {
     $('#feedback-container').prepend(
-    	feedbackTemplate(feedbackTextarea.value, date.toLocaleDateString(), date.toLocaleTimeString(), username)
+        feedbackTemplate(feedback[0], feedback[1], feedback[2], feedback[3])
     );
+}
 
+function clearUI() {
     feedbackTextarea.value = "";
+}
+
+
+ if (window.applicationCache) {
+    window.addEventListener('online', function (e) {
+        reportNetworkStatus();
+        displayFromLocalStorage();
+    }, true);
+
+    window.addEventListener('offline', function (e) {
+        reportNetworkStatus();
+    }, true);
+
+    displayFromLocalStorage();
 }
